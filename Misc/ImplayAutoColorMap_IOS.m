b82f47539caa67dda86fe5db4e945c07217c7da5
KL-Turner <klt8@psu.edu>
2020-11-03 08:41:45 -0500

    .

--- Misc/ImplayAutoColorMap_IOS.m
+++
@@ -1,9 +0,0 @@
-function [] = ImplayAutoColorMap(image, min, max)
-
-handle = implay(image);
-
-handle.Visual.ColorMap.UserRange = 1; 
-handle.Visual.ColorMap.UserRangeMin = min; 
-handle.Visual.ColorMap.UserRangeMax = max;
-
-end
