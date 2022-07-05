e4128f8e5735e25f1fa3842f96512d2768c445f0
Turner Jr <klt8@psu.edu>
2022-07-05 15:42:02 -0400

    .

--- pupilTest.m
+++
@@ -1,9 +0,0 @@
-clear; clc;
-curDir = cd;
-animalIDs = {'T99','T101','T102','T103','T105','T108','T109','T110','T111','T119','T120','T121','T122','T123'};
-for aa = 1:length(animalIDs)
-    animalID = animalIDs{1,aa};
-    cd([animalID '/Bilateral Imaging/']);
-    StageThreeProcessing_GheresTBD()
-    cd(curDir)
-end
