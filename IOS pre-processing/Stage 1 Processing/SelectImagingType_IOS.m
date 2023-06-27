8b92ac4d10d3b70c0b2965cdc12788a87e332382
Turner <kturner8@ad.brown.edu>
2023-06-27 10:38:06 -0400

    .

---
+++ IOS pre-processing/Stage 1 Processing/SelectImagingType_IOS.m
@@ -0,0 +1,17 @@
+function [imagingType] = SelectImagingType_IOS(imagingOptions)
+%----------------------------------------------------------------------------------------------------------
+% Written by Kevin L. Turner
+% The Pennsylvania State University, Dept. of Biomedical Engineering
+% https://github.com/KL-Turner
+%----------------------------------------------------------------------------------------------------------
+tf = 0;
+while tf == 0
+    disp('Select imaging type'); disp(' ')
+    [indx,tf] = listdlg('PromptString',{'Select an option',''},'SelectionMode','single','ListString',imagingOptions);
+    if tf == 0
+        disp('Please select an imaging option'); disp(' ');
+    else
+        disp(['Option selected: ' num2str(imagingOptions{1,indx}) ' selected']); disp(' ')
+    end
+end
+imagingType = imagingOptions{1,indx};
